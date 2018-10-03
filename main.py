a6f39f999daca9f304e67f1e2fee4702836acf1d
ana <ana_luisavs@hotmail.com>
2018-10-03 17:18:02 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,23 @@
+numFitas = input()
+numEstados, tamAlfEntrada, tamAlfFita, numTransicoes = [int(i.strip()) for i in input().strip().split(' ')]
+estados = input()
+alfEntrada = input()
+alfFita = input()
+
+transicoes = []
+
+for i in range(numTransicoes):
+    transicoes.append(input())
+
+entrada = input()
+
+print(numFitas)
+print(numEstados)
+print(tamAlfEntrada)
+print(tamAlfFita)
+print(numTransicoes)
+print(estados) #pegar o final e o inicial como o último e o primeiro
+print(alfEntrada)
+print(alfFita)
+print(transicoes)
+print(entrada)
